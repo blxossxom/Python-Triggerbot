1eaf4edcaf52178d0e19e759485238e6efc38c31
blxossxom <108165343+blxossxom@users.noreply.github.com>
2022-06-30 10:19:14 +1000

    Update triggerbot.py

--- triggerbot.py
+++ triggerbot.py
@@ -20,17 +20,3 @@ def trigger_bot():
                         else:
                             time.sleep(dpg.get_value('triggerbot_delay'))
                         lp.force_attack(6)
-                
-                if dpg.get_value('auto_zeus_checkbox'):
-                    if lp.active_weapon() == 31 and lp.get_team_by_crosshair(entity) != ent.get_team(lp.local_player()):
-                        if lp.get_health_by_crosshair(entity) > 0:
-                            if distance <= 150:
-                                lp.force_attack(6)
-
-                if dpg.get_value('knifebot_checkbox'):
-                    if weapon_knife(lp.active_weapon()) and lp.get_team_by_crosshair(entity) != ent.get_team(lp.local_player()):
-                        if distance <= 82:
-                            if lp.get_health_by_crosshair(entity) <= 55 and distance <= 70:
-                                lp.force_attack2(6)
-                            else:
-                                lp.force_attack(6)
